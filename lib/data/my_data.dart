20e728a617b937ca922ba7ed769ccb5821ca604e
jorgequintero <jquintedori@gmail.com>
2024-02-01 14:48:13 -0500

    final version

---
+++ lib/data/my_data.dart
@@ -0,0 +1,3 @@
+// ignore_for_file: non_constant_identifier_names
+
+String API_KEY = "f98a3f7ff5b0adea2c8e705fe1f95f6d";
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:myapp/bloc/weather_bloc_bloc.dart';
 import 'screens/home_screen.dart';
+
 
 
 void main() {
@@ -11,9 +15,52 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen()
+    return MaterialApp(
+			debugShowCheckedModeBanner: false,
+      home: FutureBuilder(
+				future: _determinePosition(),
+        builder: (context, snap) {
+					if(snap.hasData) {
+						return BlocProvider<WeatherBlocBloc>(
+							create: (context) => WeatherBlocBloc()..add(
+								FetchWeather(snap.data as Position)
+							),
+							child: const HomeScreen(),
+						);
+					} else {
+						return const Scaffold(
+							body: Center(
+								child: CircularProgressIndicator(),
+							),
+						);
+					}
+        }
+      )
     );
   }
+}
+
+Future<Position> _determinePosition() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+
+
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    return Future.error('Location services are disabled.');
+  }
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      return Future.error('Location permissions are denied');
+    }
+  }
+
+  if (permission == LocationPermission.deniedForever) {
+    return Future.error(
+      'Location permissions are permanently denied, we cannot request permissions.');
+  }
+  return await Geolocator.getCurrentPosition();
 }
